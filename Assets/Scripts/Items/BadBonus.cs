e30d2f1c4d9ee325426cd45f6b19519c05260555
fail2843 <83495514+fail2843@users.noreply.github.com>
2021-09-21 21:45:08 +0700

    Roll-a-Ball

---
+++ Assets/Scripts/Items/BadBonus.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+internal sealed class BadBonus : InteractiveObjects
+{
+    override protected void Interaction()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Player/PlayerBall.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace PlayerDefinition
+{
+    public class PlayerBall : Player
+    {
+        private void Update() => Move();
+    }
+}
